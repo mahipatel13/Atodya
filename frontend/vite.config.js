7d80ca32c23cb785a0f5bc19f751d7a58430d810
mahipatel13 <192485184+mahipatel13@users.noreply.github.com>
2026-04-26 22:05:55 +0530

    fix routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // IMPORTANT for deployment
+  base: "/",  
+
+  server: {
+    port: 5173,
+    open: true
+  },
+
+  build: {
+    outDir: "dist",     // ensure output folder
+    sourcemap: false
+  }
 })
